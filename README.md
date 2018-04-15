1003a8149f487aa80a9fbdae55037f1fb81af847
Ivan Cherneev <cherneev91@mail.ru>
2018-04-15 17:27:53 +0300

    html-start-template 1.0.0

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-> Мой проект
+<h1>html-start-template</h1>
 
-Как установить:
+<p>HTML Template (Pug+Sass+Gulp)</p>
 
-1. git clone https://github.com/IvanCherneev/Start_Project.git
-2. npm i
-3. bower i
-4. gulp
+<h2>Как cкачать, установить и начать пользоваться:</h2>
+
+<ol>
+	<li>На компьютере должны быть уже установлены или их необходимо установить:
+		<ul>
+			<li>пакетные менеджеры npm и bower</li>
+			<li>таск-менеджер gulp</li>
+			<li>желательно, но необязательно система контроля версий Git</li>
+		</ul>
+	</li>
+	<li>Скачиваем шаблон по ссылке 
+		<a href="https://github.com/IvanCherneev/html-start-template/archive/master.zip">Загрузить</a> 
+		или же ипользуя консоль. Для загрузки через консоль, нужно указать директорию и 
+		ввести команду <b><i>git clone https://github.com/IvanCherneev/html-start-template.git</i></b></li>
+	<li>Устанавливаем npm-пакеты: <b><i>npm i</i></b></li>
+	<li>Устанавливаем bower-пакеты: <b><i>bower i</i></b></li>
+	<li>Запускаем шаблон: <b><i>gulp</i></b></li>
+</ol>
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    cssnano = require('gulp-cssnano'),
-    compass = require('gulp-compass'),
-    clean = require('gulp-clean'),
-    notify = require('gulp-notify'),
-    plumber = require('gulp-plumber'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    wiredep = require('gulp-wiredep'),
-    useref = require('gulp-useref'),
-    browserSync = require('browser-sync').create(),
-    filter = require('gulp-filter'),
-    order = require('gulp-order'),
-    mainBowerFiles = require('main-bower-files');
+"use strict";
+
+var gulp           = require('gulp'),
+    sourcemaps     = require('gulp-sourcemaps'),
+    sass           = require('gulp-sass'),
+    notify         = require('gulp-notify'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    cssnano        = require('gulp-cssnano'),
+    concat         = require('gulp-concat'),
+    rename         = require('gulp-rename'),
+    clean          = require('gulp-clean'),
+    plumber        = require('gulp-plumber'),
+    uglify         = require('gulp-uglify'),
+    browserSync    = require('browser-sync').create(),
+    filter         = require('gulp-filter'),
+    mainBowerFiles = require('main-bower-files'),
+    imagemin       = require('gulp-imagemin'),
+    cache          = require('gulp-cache'),
+    rev            = require('gulp-rev-append');
 
 // Задача запускаемая по умолчанию режим разработчика по команде Gulp 
 gulp.task('default', ['clean'], function() {
@@ -22,7 +24,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 // Задача запускаемая готовую продакшн сборку
-gulp.task('production', ['clean'], function() {
+gulp.task('prod', ['clean'], function() {
   gulp.run('build');
 });
 
@@ -34,107 +36,129 @@ gulp.task('build', ['html', 'styles', 'styles:vendor', 'scripts', 'scripts:vendo
 
 // Слежка за изменением файлов в проекте и если это произошло, то запускаются соответствующие задачи
 gulp.task('watch', function() {
-  gulp.watch('_dev/css/**/*.scss', ['styles']); // стили
-    gulp.watch('./_dev/js/**/*.js', ['scripts']); // скрипты
-    gulp.watch('_dev/index.html', ['html']); // html
-    gulp.watch('./bower.json', ['styles:vendor', 'scripts:vendor']); // Вендорные стили и скрипты
-    gulp.watch('./_dev/fonts/**/*.*', ['fonts']); // шрифты
-    gulp.watch('./_dev/images/**/*.*', ['images']); // изображения
-    gulp.watch('_dev/**/*.*').on('change', browserSync.reload); // перезапуск browserSynс
+  gulp.watch('./_dev/css/**/*.scss', ['styles']); // стили
+  gulp.watch('./_dev/js/**/*.js', ['scripts']); // скрипты
+  gulp.watch('./_dev/*.html', ['html']); // html
+  gulp.watch('./bower.json', ['styles:vendor', 'scripts:vendor']); // Вендорные стили и скрипты
+  gulp.watch('./_dev/fonts/**/*.*', ['fonts']); // шрифты
+  gulp.watch('./_dev/images/**/*.*', ['images']); // изображения
+  gulp.watch('./_dev/**/*.*').on('change', browserSync.reload); // перезапуск browserSynс
 });
 
-// Сборка и перемещение стилей в папку _production
+// Сборка и перемещение стилей в папку _prod
 gulp.task('styles', function() {
-  return gulp.src('_dev/css/styles.scss')
-    .pipe(plumber({ // ловля ошибок
-      errorHandler: notify.onError(function(err) { // отображение ошибок в удобной форме
-        return {
-          title: 'Styles',
-          message: err.message
-        }
-      })
-    }))
+  return gulp.src('./_dev/css/styles.scss')
     .pipe(sourcemaps.init()) // Начала записи файла для отладки стилей в devTools
-    .pipe(sass().on('error', sass.logError)) // Компиляция SASS
+    .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError())) // Компиляция SASS, с проверкой на ошибки
     .pipe(autoprefixer({ // Добавление автопрефиксов.
       browsers: ['last 2 versions']
     }))
-    .pipe(concat('main.min.css')) // объединение файлов стилей в один
     .pipe(cssnano()) // минификация файла стилей
+    .pipe(rename({  // переименовываем файл стилей
+      basename: "main",
+      suffix: ".min"
+    }))
     .pipe(sourcemaps.write()) // Конец записи файла для отладки в devTools
-    .pipe(gulp.dest('_production/css'));
+    .pipe(gulp.dest('./_prod/css'));
 });
 
-// Перемещение Вендорных стилей в папку _production
+// Перемещение Вендорных стилей в папку _prod
 gulp.task('styles:vendor', function () {
-  var vendors = mainBowerFiles({
+  return gulp.src(mainBowerFiles({ // Находим все bower-библиотеки(полный путь до них) 
     paths: {
       bowerDirectory: './bower_components',
+      bowerrc: './.bowerrc',
       bowerJson: './bower.json'
     }
-  });
-   
-  return gulp.src(vendors)
-     .pipe(filter(['**/*.css']))
-     .pipe(concat('vendor.css'))
-     .pipe(cssnano()) // Минификация стилей
-     .pipe(gulp.dest('_production/css'));
+  }))
+    .pipe(filter(['**/*.css'])) // Отфильтруем bower-библиотеки (только css)
+    .pipe(concat('vendor.css'))
+    .pipe(cssnano()) // Минификация стилей
+    .pipe(gulp.dest('./_prod/css'));
 });
 
-//Задача для удаление папки _production
+// Задача для удаление папки _prod
 gulp.task('clean', function() {
-  return gulp.src('_production/')
+  return gulp.src('./_prod/')
     .pipe(clean());
 });
 
-// Перемещение HTML в папку _production
-gulp.task('html', function() {
-  return gulp.src('_dev/index.html')
-    .pipe(gulp.dest('_production/'))
+// Задача на очистку кэша (запускается вручную)
+gulp.task('clearcache', function() {
+  return cache.clearAll();
 });
 
-// Перемещение скриптов в папку _production
+// Задача делает замену стилей и скриптов, которые закэшированы в браузере (запускается вручную)
+gulp.task('revAll', function() {
+  gulp.src('./_prod/index.html')
+    .pipe(rev())
+    .pipe(gulp.dest('./_prod/'));
+});
+
+// Перемещение HTML в папку _prod
+gulp.task('html', function() {
+  return gulp.src('./_dev/*.html')
+    .pipe(gulp.dest('./_prod/'));
+});
+
+// Перемещение скриптов в папку _prod
 gulp.task('scripts', function() {
-  return gulp.src(['./_dev/js/**/*.js', '!./_dev/js/vendor/modernizr.js'])
+  return gulp.src('./_dev/js/**/*.js')
+    .pipe(plumber({ // ловля ошибок
+      errorHandler: function(err) { // отображение ошибок в удобной форме
+        notify.onError({
+          title:   'ErrorScript',
+          message: err.message
+        })(err);
+        this.emit('end');
+      }
+    }))
     .pipe(concat('main.min.js'))
     .pipe(uglify()) // Минификация скриптов
-    .pipe(gulp.dest('_production/js'));
+    .pipe(gulp.dest('./_prod/js'));
 });
 
-// Перемещение Вендорных скриптов в папку _production
+// Перемещение Вендорных скриптов в папку _prod
 gulp.task('scripts:vendor', function () {
-  var vendors = mainBowerFiles({
+  return gulp.src(mainBowerFiles({ // Находим все bower-библиотеки(полный путь до них)
     paths: {
       bowerDirectory: './bower_components',
+      bowerrc: './.bowerrc',
       bowerJson: './bower.json'
     }
-  });
-   
-  return gulp.src(vendors)
-     .pipe(filter(['**/*.js']))
-     .pipe(concat('vendor.js'))
-     .pipe(uglify()) // Минификация скриптов
-     .pipe(gulp.dest('_production/js'));
+  }))
+    .pipe(filter(['**/*.js'])) //Отфильтруем bower-библиотеки (только js)
+    .pipe(concat('vendor.js'))
+    .pipe(uglify()) // Минификация скриптов
+    .pipe(gulp.dest('./_prod/js'));
 });
 
 // Задача для запуска сервера
 gulp.task('browser-sync', function() {
   return browserSync.init({
-	reloadDelay: 2000,  
+    reloadDelay: 2000,  
     server: {
-      baseDir: './_production/'
+      baseDir: './_prod/'
     }
   });
 });
 
-// Перемещение шрифтов в папку _production
+// Перемещение шрифтов в папку _prod
 gulp.task('fonts', function() {
   return gulp.src('./_dev/fonts/**/*.*')
-    .pipe(gulp.dest('./_production/fonts'));
+    .pipe(gulp.dest('./_prod/fonts'));
 });
 
-// Перемещение изображений в папку _production
+// Перемещение изображений в папку _prod
 gulp.task('images', function() {
   return gulp.src('./_dev/images/**/*.*')
-    .pipe(gulp.dest('./_production/images'));
+    .pipe(cache(imagemin({ // минификация и кэширование изображений
+      interlaced: true,
+      optimizationLevel: 3,
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }]
+    })))
+    .pipe(gulp.dest('./_prod/images'));
 });
