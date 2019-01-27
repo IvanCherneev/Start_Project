c3e7d95ec44681ce9cd0e6410a3b508ea1234cec
Ivan Cherneev <cherneev91@mail.ru>
2019-01-27 21:43:35 +0300

    gulp-v4 support added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 "use strict";
 
 /*-----------------------
+*   Дополнительные переменные
+*------------------------*/
+var gulpversion = '4' // указание версии gulp, если 3-я то нужно указать 3
+
+/*-----------------------
 *   Подключение плагинов
 -------------------------*/
 var gulp           = require('gulp'),
@@ -81,148 +86,302 @@ var bower_paths = mainBowerFiles({
 *   Указание задач
 ---------------------------*/
 
-// Запуск по умолчанию режима разработчика по команде Gulp 
-gulp.task('default', ['clean'], function() {
-  gulp.run('dev');
-});
+// Если используется 3-я версия gulp
+if (gulpversion == '3') {
 
-// Запуск продакшн сборки
-gulp.task('prod', ['clean'], function() {
-  gulp.run('build');
-});
-
-// Запуск режима разработчика
-gulp.task('dev', ['build', 'watch', 'browser-sync']);
-
-// Запуск сборки проекта
-gulp.task('build', ['pug', 'styles', 'styles:vendor', 'scripts', 'scripts:vendor', 'fonts', 'images']);
-
-// Слежка за изменением файлов в проекте и если это произошло, то запускаются соответствующие задачи
-gulp.task('watch', function() {
-  gulp.watch(path.pug.src, ['pug']);
-  gulp.watch(path.sass.src, ['styles']);
-  gulp.watch(path.js.src, ['scripts']);
-  gulp.watch('./bower.json', ['styles:vendor', 'scripts:vendor']);
-  gulp.watch(path.fonts.src, ['fonts']);
-  gulp.watch(path.images.src, ['images']);
-  gulp.watch(path.all.src).on('change', browserSync.reload);
-});
-
-// Сборка и перемещение pug-файлов 
-gulp.task('pug', function() {
-  return gulp.src(path.pug.entry)
-    .pipe(plumber({
-      errorHandler: function(err) {
-        notify.onError({
-          title:   'ErrorPug',
-          message: err.message
-        })(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(pug({
-        pretty: '\t'
-    }))
-    .pipe(gulp.dest(path.pug.dist));
-});
-
-// Сборка и перемещение стилей
-gulp.task('styles', function() {
-  return gulp.src(path.sass.entry)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on("error", notify.onError()))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(cssnano())
-    .pipe(rename({
-      basename: "main",
-      suffix: ".min"
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.sass.dist));
-});
-
-// Сборка и перемещение Вендорных стилей
-gulp.task('styles:vendor', function () {
-  return gulp.src(bower_paths)
-    .pipe(filter(['**/*.css']))
-    .pipe(concat('vendor.css'))
-    .pipe(cssnano())
-    .pipe(gulp.dest(path.sass.dist));
-});
-
-// Сборка и перемещение скриптов
-gulp.task('scripts', function() {
-  return gulp.src(path.js.src)
-    .pipe(plumber({
-      errorHandler: function(err) {
-        notify.onError({
-          title:   'ErrorScript',
-          message: err.message
-        })(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(path.js.dist));
-});
-
-// Сборка и перемещение Вендорных скриптов
-gulp.task('scripts:vendor', function () {
-  return gulp.src(bower_paths)
-    .pipe(filter(['**/*.js']))
-    .pipe(concat('vendor.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(path.js.dist));
-});
-
-// Перемещение шрифтов
-gulp.task('fonts', function() {
-  return gulp.src(path.fonts.src)
-    .pipe(gulp.dest(path.fonts.dist));
-});
-
-// Оптимизация и перемещение изображений
-gulp.task('images', function() {
-  return gulp.src(path.images.src)
-    .pipe(cache(imagemin({
-      interlaced: true,
-      optimizationLevel: 3,
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }]
-    })))
-    .pipe(gulp.dest(path.images.dist));
-});
-
-// Запуск сервера
-gulp.task('browser-sync', function() {
-  return browserSync.init({
-    reloadDelay: 1000,  
-    server: {
-      baseDir: PROD_DIR
-    }
+  // Запуск по умолчанию режима разработчика по команде Gulp
+  gulp.task('default', ['clean'], function() {
+    gulp.run('dev');
   });
-});
 
-// Удаление продакшн директории
-gulp.task('clean', function() {
-  return gulp.src(PROD_DIR)
-    .pipe(clean());
-});
+  // Запуск продакшн сборки
+  gulp.task('prod', ['clean'], function() {
+    gulp.run('build');
+  });
 
-// Запуск очистку кэша (запускается вручную)
-gulp.task('clearcache', function() {
-  return cache.clearAll();
-});
+  // Запуск режима разработчика
+  gulp.task('dev', ['build', 'watch', 'browser-sync']);
 
-// Запуск замены стилей и скриптов, которые закэшированы в браузере (запускается вручную)
-gulp.task('revAll', function() {
-  gulp.src(path.html.entry)
-    .pipe(rev())
-    .pipe(gulp.dest(path.html.dist));
-});
+  // Запуск сборки проекта
+  gulp.task('build', ['pug', 'styles', 'styles:vendor', 'scripts', 'scripts:vendor', 'fonts', 'images']);
+
+  // Слежка за изменением файлов в проекте и если это произошло, то запускаются соответствующие задачи
+  gulp.task('watch', function() {
+    gulp.watch(path.pug.src, ['pug']);
+    gulp.watch(path.sass.src, ['styles']);
+    gulp.watch(path.js.src, ['scripts']);
+    gulp.watch('./bower.json', ['styles:vendor', 'scripts:vendor']);
+    gulp.watch(path.fonts.src, ['fonts']);
+    gulp.watch(path.images.src, ['images']);
+    gulp.watch(path.all.src).on('change', browserSync.reload);
+  });
+
+  // Сборка и перемещение pug-файлов 
+  gulp.task('pug', function() {
+    return gulp.src(path.pug.entry)
+      .pipe(plumber({
+        errorHandler: function(err) {
+          notify.onError({
+            title:   'ErrorPug',
+            message: err.message
+          })(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(pug({
+          pretty: '\t'
+      }))
+      .pipe(gulp.dest(path.pug.dist));
+  });
+
+  // Сборка и перемещение стилей
+  gulp.task('styles', function() {
+    return gulp.src(path.sass.entry)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", notify.onError()))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(cssnano())
+      .pipe(rename({
+        basename: "main",
+        suffix: ".min"
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.sass.dist));
+  });
+
+  // Сборка и перемещение Вендорных стилей
+  gulp.task('styles:vendor', function() {
+    return gulp.src(bower_paths)
+      .pipe(filter(['**/*.css']))
+      .pipe(concat('vendor.css'))
+      .pipe(cssnano())
+      .pipe(gulp.dest(path.sass.dist));
+  });
+
+  // Сборка и перемещение скриптов
+  gulp.task('scripts', function() {
+    return gulp.src(path.js.src)
+      .pipe(plumber({
+        errorHandler: function(err) {
+          notify.onError({
+            title:   'ErrorScript',
+            message: err.message
+          })(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(path.js.dist));
+  });
+
+  // Сборка и перемещение Вендорных скриптов
+  gulp.task('scripts:vendor', function() {
+    return gulp.src(bower_paths)
+      .pipe(filter(['**/*.js']))
+      .pipe(concat('vendor.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(path.js.dist));
+  });
+
+  // Перемещение шрифтов
+  gulp.task('fonts', function() {
+    return gulp.src(path.fonts.src)
+      .pipe(gulp.dest(path.fonts.dist));
+  });
+
+  // Оптимизация и перемещение изображений
+  gulp.task('images', function() {
+    return gulp.src(path.images.src)
+      .pipe(cache(imagemin({
+        interlaced: true,
+        optimizationLevel: 3,
+        progressive: true,
+        svgoPlugins: [{
+          removeViewBox: false
+        }]
+      })))
+      .pipe(gulp.dest(path.images.dist));
+  });
+
+  // Запуск сервера
+  gulp.task('browser-sync', function() {
+    return browserSync.init({
+      reloadDelay: 1000,  
+      server: {
+        baseDir: PROD_DIR
+      }
+    });
+  });
+
+  // Удаление продакшн директории
+  gulp.task('clean', function() {
+    return gulp.src(PROD_DIR)
+      .pipe(clean());
+  });
+
+  // Запуск очистку кэша (запускается вручную)
+  gulp.task('clearcache', function() {
+    return cache.clearAll();
+  });
+
+  // Запуск замены стилей и скриптов, которые закэшированы в браузере (запускается вручную)
+  gulp.task('revAll', function() {
+    gulp.src(path.html.entry)
+      .pipe(rev())
+      .pipe(gulp.dest(path.html.dist));
+  }); 
+
+}
+/*---------------------------*/
+
+// Если используется 4-я версия gulp
+if (gulpversion == '4') {
+
+  // Запуск очистку кэша (запускается вручную)
+  gulp.task('clearcache', function() {
+    return cache.clearAll();
+  });
+
+  // Запуск замены стилей и скриптов, которые закэшированы в браузере (запускается вручную)
+  gulp.task('revAll', function() {
+    gulp.src(path.html.entry)
+      .pipe(rev())
+      .pipe(gulp.dest(path.html.dist));
+  });
+
+ // Удаление продакшн директории
+  gulp.task('clean', function() {
+    return gulp.src(PROD_DIR)
+      .pipe(clean());
+  });
+
+  // Сборка и перемещение pug-файлов 
+  gulp.task('pug', function() {
+    return gulp.src(path.pug.entry)
+      .pipe(plumber({
+        errorHandler: function(err) {
+          notify.onError({
+            title:   'ErrorPug',
+            message: err.message
+          })(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(pug({
+          pretty: '\t'
+      }))
+      .pipe(gulp.dest(path.pug.dist));
+  });
+
+  // Сборка и перемещение стилей
+  gulp.task('styles', function() {
+    return gulp.src(path.sass.entry)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", notify.onError()))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(cssnano())
+      .pipe(rename({
+        basename: "main",
+        suffix: ".min"
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.sass.dist));
+  });
+
+  // Сборка и перемещение Вендорных стилей
+  gulp.task('styles:vendor', function () {
+    return gulp.src(bower_paths)
+      .pipe(filter(['**/*.css']))
+      .pipe(concat('vendor.css'))
+      .pipe(cssnano())
+      .pipe(gulp.dest(path.sass.dist));
+  });
+
+  // Сборка и перемещение скриптов
+  gulp.task('scripts', function() {
+    return gulp.src(path.js.src)
+      .pipe(plumber({
+        errorHandler: function(err) {
+          notify.onError({
+            title:   'ErrorScript',
+            message: err.message
+          })(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(path.js.dist));
+  });
+
+  // Сборка и перемещение Вендорных скриптов
+  gulp.task('scripts:vendor', function () {
+    return gulp.src(bower_paths)
+      .pipe(filter(['**/*.js']))
+      .pipe(concat('vendor.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(path.js.dist));
+  });
+
+  // Перемещение шрифтов
+  gulp.task('fonts', function() {
+    return gulp.src(path.fonts.src)
+      .pipe(gulp.dest(path.fonts.dist));
+  });
+
+  // Оптимизация и перемещение изображений
+  gulp.task('images', function() {
+    return gulp.src(path.images.src)
+      .pipe(cache(imagemin({
+        interlaced: true,
+        optimizationLevel: 3,
+        progressive: true,
+        svgoPlugins: [{
+          removeViewBox: false
+        }]
+      })))
+      .pipe(gulp.dest(path.images.dist));
+  });
+
+  // Запуск сборки проекта
+  gulp.task('build', gulp.parallel('pug', 'styles', 'styles:vendor', 'scripts', 'scripts:vendor', 'fonts', 'images'));
+
+   // Слежка за изменением файлов в проекте и если это произошло, то запускаются соответствующие задачи
+  gulp.task('watch', function() {
+    gulp.watch(path.pug.src, gulp.parallel('pug'));
+    gulp.watch(path.sass.src, gulp.parallel('styles'));
+    gulp.watch(path.js.src, gulp.parallel('scripts'));
+    gulp.watch('./bower.json', gulp.parallel('styles:vendor', 'scripts:vendor'));
+    gulp.watch(path.fonts.src, gulp.parallel('fonts'));
+    gulp.watch(path.images.src, gulp.parallel('images'));
+    gulp.watch(path.all.src).on('change', browserSync.reload);
+  });
+
+  // Запуск сервера
+  gulp.task('browser-sync', function() {
+    return browserSync.init({
+      reloadDelay: 1000,  
+      server: {
+        baseDir: PROD_DIR
+      }
+    });
+  });
+
+  // Запуск режима разработчика
+  gulp.task('dev', gulp.parallel('build', 'watch', 'browser-sync'));
+
+  // Запуск продакшн сборки
+  gulp.task('prod', gulp.series('clean', 'build'));
+
+  // Запуск по умолчанию режима разработчика по команде Gulp
+  gulp.task('default', gulp.series('clean', 'dev'));
+
+}
+/*----------------------------*/
+
 
